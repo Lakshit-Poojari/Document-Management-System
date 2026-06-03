84860325dd962a09f78633dd66da7df3ad3d9b3e
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-03 15:45:28 +0530

    create categoryModel

---
+++ backend/models/categoryModel.ts
@@ -0,0 +1,63 @@
+
+import { db } from "../lib/db";
+import { CreateCategory } from "../types/categoryTypes";
+
+export async function createCategory(category: CreateCategory) {
+    try{
+        const [result] = await db.execute("INSERT INTO categories (category_name, description) VALUES (?, ?)",
+            [category.category_name, category.description ?? null]
+        );
+        return result;  
+    }
+    catch (error) {
+        console.error("Error creating category(model):", error);
+        throw error;
+    }
+}
+
+export async function getCategories() {
+    try{
+        const [categories] = await db.execute("SELECT * FROM categories");
+        return categories;
+    }
+    catch (error) {
+        console.error("Error fetching categories(model):", error);
+        throw error;
+    }
+}
+
+export async function getCategoryById(category_id: number) {
+    try{
+        const [category] = await db.execute("SELECT * FROM categories WHERE category_id = ? ", [category_id]);
+        return category
+    }
+    catch (error) {
+        console.error("Error fetching category by ID(model):", error);
+        throw error;
+    }
+}
+
+export async function updateCategory(category_id: number, category: CreateCategory) {
+    try{
+        const [result] = await db.execute(
+            "UPDATE categories SET category_name=?, description = ? WHERE category_id = ?",
+             [category.category_name, category.description ?? null, category_id])
+        return result
+    }
+    catch (error) {
+        console.error("Error updating category(model):", error);
+        throw error;
+    }
+}
+
+export async function deleteCategory(category_id: number) {
+    try{
+        const [result] = await db.execute("DELETE FROM categories WHERE category_id = ?", [category_id])
+
+        return result
+    }
+    catch (error) {
+        console.error("Error deleting category(model):", error);
+        throw error;
+    }
+}
