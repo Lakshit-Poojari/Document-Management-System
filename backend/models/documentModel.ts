ae5159bded00063fd5c60834f546880309a9910a
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-03 17:58:41 +0530

    create document model

---
+++ backend/models/documentModel.ts
@@ -0,0 +1,77 @@
+import db from "../lib/db";
+import { Document } from "../types/documentTypes";
+
+export async function createDocument(document: Document) {
+    try {
+        const [result] =await db.execute
+        (`INSERT INTO documents 
+            (title, description, file_name, file_path, file_type, file_size,  uploaded_by, category_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?) `,
+            [
+                document.title, document.description ?? null, document.file_name, document.file_path,
+                document.file_type ?? null, document.file_size ?? null, document.uploaded_by, document.category_id
+            ]
+        )
+
+        return result
+
+    } catch (error) {
+        console.error("Error creating document(model):", error);
+        throw error;
+    }
+}
+
+export async function getSingleDocument(documentId: number) {
+    try {
+        const [result] =await db.execute("SELECT * FROM documents WHERE document_id= ?", [documentId])
+
+        return result
+    } catch (error) {
+        console.error("Error creating document(model):", error);
+        throw error;
+    }
+}
+
+export async function getDocuments() {
+    try {
+        const [result] =await db.execute("SELECT * FROM documents ORDER BY document_id DESC")
+
+        return result
+    } catch (error) {
+        console.error("Error creating document(model):", error);
+        throw error;
+    }
+}
+
+export async function updateDocument(documentId: number, document: Document) {
+    try {
+        const [result] =await db.execute(`UPDATE documents SET 
+                title = ?, description = ?, file_name = ?,
+                file_path = ?, file_type = ?, file_size = ?,
+                category_id = ?
+            WHERE document_id = ?`, [document.title,
+                document.description ?? null,
+                document.file_name,
+                document.file_path,
+                document.file_type ?? null,
+                document.file_size ?? null,
+                document.category_id,
+                documentId])
+
+        return result
+    } catch (error) {
+        console.error("Error creating document(model):", error);
+        throw error;
+    }
+}
+
+export async function deleteDocument(documentId: number) {
+    try {
+        const [result] =await db.execute("DELETE FROM documents WHERE document_id=?", [documentId])
+
+        return result
+    } catch (error) {
+        console.error("Error creating document(model):", error);
+        throw error;
+    }
+}
