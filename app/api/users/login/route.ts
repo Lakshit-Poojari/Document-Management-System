6d8075e507d067d2320a5dec86f2a59808f7f61f
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-05 11:24:07 +0530

    auth routes

---
+++ app/api/users/login/route.ts
@@ -0,0 +1,14 @@
+import { loginController } from "@/backend/controllers/userController";
+import { NextResponse } from "next/server";
+
+export async function POST(request: NextResponse) {
+    try {
+        const body = await request.json()
+
+        const result = await loginController(body)
+
+        return NextResponse.json(result)
+    } catch (error) {
+        return NextResponse.json({message: error instanceof Error? error.message: "Something went wrong"},{status: 400})
+    }
+}
