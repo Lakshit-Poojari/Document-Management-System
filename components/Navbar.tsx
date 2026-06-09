37d09b3a36a994ce9aba108d1dd48110eaf0600b
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-09 12:24:19 +0530

    testing logout button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
-import Link from "next/link"
+"use client";
 
+import Link from "next/link"
+import { useState } from "react"
 
 export default function Navbar() {
+    const [login, setlogin] = useState(false)
+
+    const auth = login
   return (
     <>
         <header className="sticky top-0 z-50 bg-white  shadow-md  border-b border-gray-200 ">
@@ -27,15 +32,25 @@ export default function Navbar() {
                 </div>
 
                 {/* Authentication Buttons */}
-                <div className="flex gap-4">
 
-                    <Link href="/login" className="px-4 py-2 border border-amber-500 rounded-lg hover:bg-amber-50  ">Login</Link>
+                {
 
-                    <Link href="/register" className="px-4 py-2 bg-amber-500 text-white rounded-lg hover:bg-amber-600">Register</Link>
+                    auth ? (
+                        <div className="flex gap-4">
 
-                </div>
+                            <Link href="/login" className="px-4 py-2 border border-amber-500 rounded-lg hover:bg-amber-50  ">Logout</Link>
 
+                        </div>
+                    ):
+                    (
+                    <div className="flex gap-4">
 
+                        <Link href="/login" className="px-4 py-2 border border-amber-500 rounded-lg hover:bg-amber-50  ">Login</Link>
+
+                        <Link href="/register" className="px-4 py-2 bg-amber-500 text-white rounded-lg hover:bg-amber-600">Register</Link>
+
+                    </div>
+                )}
 
             </nav>
         </header>
