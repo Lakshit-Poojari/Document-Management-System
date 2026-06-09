cddc2ebe67545c1985882271c4421ecb756f8324
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-09 17:02:24 +0530

    remove un wanted code

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -1,20 +1,3 @@
-// import jwt from "jsonwebtoken";
-// import { SECRET_KEY } from "../lib/auth";
-
-// export interface JwtPayload {
-//   user_id: number;
-//   email: string;
-//   role: string;
-// }
-
-// export function verifyToken( token: string): JwtPayload {
-//   try {
-//     return jwt.verify( token, SECRET_KEY) as JwtPayload;
-//   } catch {
-//     throw new Error("Invalid Token");
-//   }
-// }
-
 import jwt from "jsonwebtoken";
 import { SECRET_KEY } from "../lib/auth";
 
