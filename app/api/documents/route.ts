4e4754a0b7b893667b53b4870adf4d9124890c1c
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-10 15:32:45 +0530

    update api

--- app/api/documents/route.ts
+++ app/api/documents/route.ts
@@ -1,17 +1,58 @@
 import { createDocumentController, getAllDocumentController } from "@/backend/controllers/documentController";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 
-export async function POST(request:NextResponse){
-    try {
-        const body = await request.json()
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData();
 
-        const result = await createDocumentController(body)
+    const file = formData.get("file") as File;
 
-        return NextResponse.json(result)
-    } catch (error) {
-        return NextResponse.json( {message:error}, {status: 400})
+    if (!file) {
+      return NextResponse.json(
+        { message: "File is required" },
+        { status: 400 }
+      );
     }
+
+    // TODO: Get uploaded_by from JWT/cookie
+    const uploaded_by = 1;
+
+    const body = {
+      title: formData.get("title") as string,
+      description: formData.get("description") as string,
+
+      file_name: file.name,
+      file_path: `/uploads/${file.name}`, // temporary
+
+      file_type: file.type,
+      file_size: file.size,
+
+      uploaded_by,
+
+      category_id: Number(formData.get("category_id")),
+    };
+
+    const result = await createDocumentController(body);
+
+    return NextResponse.json(result, {
+      status: 201,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        message:
+          error instanceof Error
+            ? error.message
+            : "Something went wrong",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
 }
 
 export async function GET(){
