55cf381957e2d7619b362d937f68d944ff990de2
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-06-10 15:34:47 +0530

    update backend

--- backend/services/documentService.ts
+++ backend/services/documentService.ts
@@ -2,19 +2,31 @@ import { createDocumentModel, deleteDocumentModel,
          getAllDocumentModel, getSingleDocumentModel, updateDocumentModel } from "../models/documentModel";
 import { CreateDocument } from "../types/documentTypes";
 
-export async function createDocumentService(document: CreateDocument) {
-    try {
-        if (!document.title || !document.file_name || !document.file_path || !document.uploaded_by || !document.category_id) {
-            throw new Error("Required fields are missing");
-        }
-
-        const result = await createDocumentModel(document)
-
-        return [result]   
-    } catch (error) {
-        console.error("Error in creating document(service):", error);
-        throw error
+export async function createDocumentService(
+  document: CreateDocument
+) {
+  try {
+    if (
+      !document.title ||
+      !document.file_name ||
+      !document.file_path ||
+      !document.uploaded_by ||
+      !document.category_id
+    ) {
+      throw new Error("Required fields are missing");
     }
+
+    const result = await createDocumentModel(document);
+
+    return {
+      success: true,
+      message: "Document created successfully",
+      result,
+    };
+  } catch (error) {
+    console.error("Error creating document(service):", error);
+    throw error;
+  }
 }
 
 export async function getAllDocumentService() {
